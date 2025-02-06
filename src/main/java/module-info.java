1db73edec11135b525804aa5a5a60a094a0e6f62
Leck995 <yiqiu995@gmail.com>
2025-02-06 17:59:31 +0800

    切换日志为logback

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,32 +1,39 @@
-module cn.tealc.kkmaid {
+open module cn.tealc.kkmaid {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.swing;
+    requires javafx.media;
+    requires atlantafx.base;
+    requires com.jfoenix;
+    requires uk.co.caprica.vlcj;
+    requires org.controlsfx.controls;
+
+    requires jdk.crypto.cryptoki;
     requires com.fasterxml.jackson.core;
     requires com.fasterxml.jackson.databind;
     requires com.fasterxml.jackson.annotation;
-    requires org.controlsfx.controls;
-    requires atlantafx.base;
     requires eventbus.java;
-    requires com.jfoenix;
-    requires javafx.media;
-    requires jdk.crypto.cryptoki;
-    requires org.slf4j;
-    requires uk.co.caprica.vlcj;
     requires jaudiotagger;
     requires JRegistry;
     requires net.coobird.thumbnailator;
     requires filters;
-    requires javafx.swing;
+
+    requires org.slf4j;
+    requires ch.qos.logback.classic;
+
+
 
     requires org.kordamp.ikonli.core;
     requires org.kordamp.ikonli.javafx;
-    // add icon pack modules
     requires org.kordamp.ikonli.material2;
     requires java.net.http;
     requires org.apache.commons.compress;
+    requires org.apache.commons.io;
+    requires org.apache.commons.lang3;
+    requires org.apache.commons.codec;
+    requires java.naming;
 
-    opens cn.tealc995.kkmaid to javafx.fxml;
-    opens cn.tealc995.kkmaid.ui to javafx.fxml;
+
     exports cn.tealc995.kkmaid;
     exports cn.tealc995.kikoreu;
     exports cn.tealc995.kikoreu.model;
@@ -38,5 +45,4 @@ module cn.tealc.kkmaid {
     exports cn.tealc995.aria2;
     exports cn.tealc995.kikoreu.api;
     exports cn.tealc995.kkmaid.config;
-    opens cn.tealc995.kkmaid.config to javafx.fxml;
 }
